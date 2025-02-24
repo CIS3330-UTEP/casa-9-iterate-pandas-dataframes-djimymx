7268ae54243657931cbc502b9cff7440f53bfe63
Jaime Ruiz <djimymx@hotmail.com>
2025-02-23 21:55:24 -0700

    Update casa_8.py

--- casa_8.py
+++ casa_8.py
@@ -1,1 +1,14 @@
+import pandas as pd
 
+df = pd.read_csv('big-mac-full-index.csv')
+
+# Iterating through a DataFrame using iterrows()
+for index, row in df.iterrows():
+    print(f"Index: {index}, Name: {row['name']}, Local Price: {row['local_price']}, Dollar Price: {row['dollar_price']}")
+
+# Iterating through a DataFrame using apply()
+def get_country_name(row):
+    return f"Name: {row['name']}, Local Price: {row['local_price']}, Dollar Price: {row['dollar_price']}"
+    
+result = df.apply(get_country_name, axis=1)
+print(result)
